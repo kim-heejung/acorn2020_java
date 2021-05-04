bdb34d8d1474ef1adfe797b28da49d65ace676bc
kimheejung <kims9602@naver.com>
2021-05-04 14:43:29 +0900

    Step04 에 myMain 추가함

--- Step04_Class/src/test/study/MyMain01.java
+++ Step04_Class/src/test/study/MyMain01.java
@@ -4,10 +4,16 @@ import test.mypac.Car; // mypan 폴더에 만든 Car 클래스 가져옴.
 
 public class MyMain01 {
 	public static void main(String[] args) {
+		// field
+		Car c3;
+		
 		System.out.println("main 메소드가 시작되었습니다.");
-		Car cl=new Car(); // Car 적고 ctrl, 스페이스바 키 누르면 자동으로 Car 클래스 가져올 수 있음.
+		// local variable c1, c2
+		Car c1=new Car(); // Car 적고 ctrl, 스페이스바 키 누르면 자동으로 Car 클래스 가져올 수 있음.
 		Car c2=new Car();
 		// 각각의 두개의 객체를 만든거라서 id 값이 다름
 		// 같은 값을 가져왔지만 다른 값이 들어 있을 수 있음
+		c1.name="소나타";
+		c2.name="그랜저";
 	}
 }
