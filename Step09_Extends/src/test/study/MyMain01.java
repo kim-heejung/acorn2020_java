8854bcad11ffdb835ab333702d6c6accb510a8e8
kimheejung <kims9602@naver.com>
2021-05-06 18:29:23 +0900

    Step09 에 MyMain 추가함

---
+++ Step09_Extends/src/test/study/MyMain01.java
@@ -0,0 +1,21 @@
+package test.study;
+
+import test.mypac.HandPhone;
+import test.mypac.Phone;
+import test.mypac.SmartPhone;
+
+public class MyMain01 {
+	public static void main(String[] args) {
+		Phone p1=new Phone();
+		HandPhone p2=new HandPhone();
+		
+		SmartPhone p3=new SmartPhone();
+		// Phone < HandPhone < SmartPhone 
+		// 세가지가 하나의 참조값임 (같이 만들어짐) 
+		// Phone => this,  HandPhone, SmartPhone => super
+		p3.call();
+		p3.mobileCall();
+		p3.doInternet();
+		p3.takePicture();
+	}
+}
